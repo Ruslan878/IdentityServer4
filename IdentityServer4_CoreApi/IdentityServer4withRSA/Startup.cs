b4e3104fd77a8a173b8915932d4057333636e0a5
Ruslan Borovok <borovok.ruslan@outlook.com>
2017-08-08 13:18:28 +0300

    Created Identity Server project.

---
+++ IdentityServer4_CoreApi/IdentityServer4withRSA/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityServer4withRSA
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddTemporarySigningCredential()
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddInMemoryClients(Config.GetClients());
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Information);
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseIdentityServer();
+        }
+    }
+}
